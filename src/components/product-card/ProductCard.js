bc129bf08d24ed75046e0c433c27aa17ba068485
Dipak 2056 <paudelsantosh508@gmail.com>
2022-08-17 20:42:32 +1000

    some bug fixed

--- src/components/product-card/ProductCard.js
+++ src/components/product-card/ProductCard.js
@@ -1,6 +1,5 @@
 import "./product-card.css";
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const ProductCard = ({
   id,
@@ -13,53 +12,46 @@ export const ProductCard = ({
   ratedby,
 }) => {
   return (
-    <Link
-      to={`/products/${id}`}
-      style={{ textDecoration: "none", color: "black" }}
-    >
-      <div className="product__card">
-        <div className="product__card--header">{category ? category : ""}</div>
-        <div className="product__card--body">
-          <div className="product--image">
-            <img src={image} alt="product" />
-          </div>
-          <div className="product__card--product--name align-center">
-            <strong>{title}</strong>
-          </div>
-          <div className="product__card--description">
-            <strong>Description:</strong> {description}
-          </div>
-          <div className="product__card--ratings ">
-            <strong>Ratings: </strong>
+    <div className="product__card">
+      <div className="product__card--header">{category ? category : ""}</div>
+      <div className="product__card--body">
+        <div className="product--image">
+          <img src={image} alt="product" />
+        </div>
+        <div className="product__card--product--name align-center">
+          <strong>{title}</strong>
+        </div>
+        <div className="product__card--description">
+          <strong>Description:</strong> {description}
+        </div>
+        <div className="product__card--ratings ">
+          <strong>Ratings: </strong>
+          <span>
+            {" "}
             <span>
               {" "}
-              <span>
-                {" "}
-                {rating}
-                {new Array(Math.floor(rating)).fill(0).map((item) => (
-                  <i className="fa-solid fa-star text-warning" />
-                ))}
-              </span>
-              {ratedby ? `rated by:(${ratedby}) customers` : ""}
+              <i className="fa-solid fa-star text-warning" />
+              {rating}
             </span>
-          </div>
-          <div className="product__card--price">
-            {" "}
-            <strong>Price: </strong>
-            {price}
-          </div>
+            {ratedby ? `rated by:(${ratedby}) customers` : ""}
+          </span>
         </div>
-        <div className="product__card--footer">
-          <div className="shop-now-button">
-            <button>Shop Now</button>
-          </div>
-          <div className="shop-now-button">
-            <button>
-              Add to cart <i className="fa-solid fa-cart-shopping"></i>
-            </button>
-          </div>
+        <div className="product__card--price">
+          {" "}
+          <strong>Price: </strong>
+          {price}
         </div>
       </div>
-    </Link>
+      <div className="product__card--footer">
+        <div className="shop-now-button">
+          <button>Shop Now</button>
+        </div>
+        <div className="shop-now-button">
+          <button>
+            Add to cart <i className="fa-solid fa-cart-shopping"></i>
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
