c3deec1d735ffe576e0d442e74fccb79d9d726f2
Dipak 2056 <paudelsantosh508@gmail.com>
2022-08-17 10:48:02 +1000

    data-fetching process started

---
+++ src/helpers/axioshelper.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+const rootUrlAPI = "https://fakestoreapi.com";
+const productsAPI = rootUrlAPI + "/products";
+const categoriesAPI = productsAPI + "/categories";
+const getSingleProductAPI = productsAPI + "/:id";
+
+//there will be lots of repetition on doing individual axios get method
+//so i am creating this api, processor
+const apiProcessor = async ({ method, url }) => {
+  try {
+    const { data } = await axios({
+      method,
+      url,
+    });
+    return data;
+  } catch (error) {
+    let message = error.message;
+    console.log(message);
+  }
+};
+
+//to get all the categories from the external api
+export const getAllCategories = async () => {
+  const url = categoriesAPI;
+  return apiProcessor({ method: "get", url });
+};
+
+//to get all the products from the external api
+export const getAllProducts = async () => {
+  const url = productsAPI;
+  return apiProcessor({ method: "get", url });
+};
+
+//to get the product based on the product id
+export const getProductById = async (id) => {
+  const url = productsAPI + "/" + "id";
+  return apiProcessor({ method: "get", url });
+};
