934532870a8b094f32ec4a64bbdb54bd466edd66
Dipak 2056 <paudelsantosh508@gmail.com>
2022-08-18 16:19:04 +1000

    product-landing page, main card comp done

--- src/components/product-card/ProductCard.js
+++ src/components/product-card/ProductCard.js
@@ -1,5 +1,5 @@
 import "./product-card.css";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export const ProductCard = ({
@@ -12,59 +12,86 @@ export const ProductCard = ({
   category,
   ratedby,
 }) => {
+  const [img, setImg] = useState();
+  useEffect(() => {
+    const imageskeleton = new Image();
+    imageskeleton.src = image;
+    imageskeleton.onload = () => {
+      setTimeout(() => {
+        setImg(imageskeleton);
+      }, 400);
+    };
+  }, []);
   return (
-    <div className="product__card">
-      {category ? <div className="product__card--header">{category}</div> : ""}
-      <div className="product__card--body">
-        <Link
-          to={`/products/${id}`}
-          style={{ textDecoration: "none", color: "white" }}
-        >
-          <div className="product--image">
-            <img src={image} alt="product" />
-          </div>{" "}
-        </Link>
+    <>
+      {img ? (
+        <>
+          <div className="product__card">
+            {category ? (
+              <div className="product__card--header">{category}</div>
+            ) : (
+              ""
+            )}
+            <div className="product__card--body">
+              <Link
+                to={`/products/${id}`}
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <div className="product--image">
+                  <img src={image} alt="product" />
+                </div>{" "}
+              </Link>
 
-        <div className="product__card--product--name align-center">
-          <strong>{title}</strong>
-        </div>
-        <div className="product__card--description">
-          <strong>Description:</strong> {description}
-        </div>
-        <div className="product__card--ratings ">
-          <strong>Ratings: </strong>
-          <span>
-            {" "}
-            <span>
-              {" "}
-              <i className="fa-solid fa-star text-warning" />
-              {rating}
-            </span>
-            {ratedby ? `rated by:(${ratedby}) customers` : ""}
-          </span>
-        </div>
-        <div className="product__card--price">
-          {" "}
-          <strong>Price: </strong>
-          {price}
-        </div>
-      </div>
-      <div className="product__card--footer">
-        <div className="shop-now-button">
-          <Link
-            to={`/products/${id}`}
-            style={{ textDecoration: "none", color: "white" }}
-          >
-            {" "}
-            <button>Shop Now</button>
-          </Link>
-        </div>
-        <div className="shop-now-button">
-          <button>
-            Add to cart <i className="fa-solid fa-cart-shopping"></i>
-          </button>
-        </div>
-      </div>
-    </div>
+              <div className="product__card--product--name align-center">
+                <strong>{title}</strong>
+              </div>
+              <div className="product__card--description">
+                <strong>Description:</strong> {description}
+              </div>
+              <div className="product__card--ratings ">
+                <strong>Ratings: </strong>
+                <span>
+                  {" "}
+                  <span>
+                    {" "}
+                    <i className="fa-solid fa-star text-warning" />
+                    {rating}
+                  </span>
+                  {ratedby ? `rated by:(${ratedby}) customers` : ""}
+                </span>
+              </div>
+              <div className="product__card--price">
+                {" "}
+                <strong>Price: </strong>
+                {price}
+              </div>
+            </div>
+            <div className="product__card--footer">
+              <div className="shop-now-button">
+                <Link
+                  to={`/products/${id}`}
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  {" "}
+                  <button>Shop Now</button>
+                </Link>
+              </div>
+              <div className="shop-now-button">
+                <button>
+                  Add to cart <i className="fa-solid fa-cart-shopping"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="product__img--skeleton"> </div>
+          <div className="skeleton product__title--skeleton"></div>
+          <div className="skeleton product__rating--skeleton"></div>
+          <div className="skeleton product__price--skeleton"></div>
+        </>
+      )}
+    </>
   );
 };
--- src/pages/product-landingPage/ProductLandingPage.jsx
+++ src/pages/product-landingPage/ProductLandingPage.jsx
@@ -2,52 +2,49 @@ import React from "react";
 import "./ProductLandingPage.css";
 import { ProductCard } from "../../components/product-card/ProductCard";
 import DefaultLayout from "../../components/layouts/DefaultLayout";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchProductsAction } from "../LandingPage/products.action";
 
 const ProductLandingPage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchProductsAction());
+  }, []);
+  const { products } = useSelector((state) => state.product);
+
+  const { id } = useParams();
+  const product = products.find((product) => +product.id === +id);
+  console.log(id);
+  console.log(products);
   return (
     <DefaultLayout>
       <section className="productlanding__main">
         <div className="productlanding__card">
           <div className="productlanding--img">
-            <img
-              src="https://dduhxx0oznf63.cloudfront.net/images/products/300x300/461781023_f8676288bed6b97.jpg"
-              alt=""
-            />
+            <img src={product.image} alt="" />
           </div>
           <div className="productlanding--description">
             <>
               <div className="product__landing--product--title">
-                People Shampoo or conditioner
+                {product.title}
               </div>
               <div className="product__landing--product--description">
-                Very good product with no side effect. Easy to apply.
+                {product.description}
               </div>
               <div className="productlanding--description--imgs">
                 <div className="productlanding--description--smallimg">
-                  <img
-                    src="https://dduhxx0oznf63.cloudfront.net/images/products/300x300/461781023_f8676288bed6b97.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="productlanding--description--smallimg">
-                  <img
-                    src="https://dduhxx0oznf63.cloudfront.net/images/products/300x300/461781023_f8676288bed6b97.jpg"
-                    alt=""
-                  />
-                </div>
-                <div className="productlanding--description--smallimg">
-                  <img
-                    src="https://dduhxx0oznf63.cloudfront.net/images/products/300x300/461781023_f8676288bed6b97.jpg"
-                    alt=""
-                  />
+                  <img src={product.image} alt="small image" />
                 </div>
               </div>
               <div className="product__landing--product--price">
-                <strong>Price:</strong> $8.00
+                <strong>Price:</strong> ${product.price}
               </div>
               <div className="product__landing--product--price">
-                <strong>Ratings:</strong>{" "}
+                <strong>Ratings:{product.rating.rate}</strong>
                 <i className="fa-solid fa-star text-warning"></i>
+                Rated over {product.rating.count} times.
               </div>
             </>
             <button className="product--card--footer">
