7fe2e25f634e516186f9ae2f102af50ea3d327db
Dipak 2056 <paudelsantosh508@gmail.com>
2022-08-18 14:52:43 +1000

    made some changes with animations

--- src/AnimatedPage.jsx
+++ src/AnimatedPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const animations = {
-  initial: { opacity: 0.5, x: 100 },
+  initial: { opacity: 0.5, x: 0 },
   animate: { opacity: 1, x: 0 },
   exit: { opacity: 0, x: -100 },
 };
