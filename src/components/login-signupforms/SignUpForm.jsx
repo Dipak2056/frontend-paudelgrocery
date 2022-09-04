8925c63f09ac6011bbdeb5a24d146687ee779a22
Dipak 2056 <paudelsantosh508@gmail.com>
2022-09-04 12:40:32 +1000

    signup functionality with simple error message shown

--- src/components/login-signupforms/SignUpForm.jsx
+++ src/components/login-signupforms/SignUpForm.jsx
@@ -1,18 +1,61 @@
 import React from "react";
+import { useState } from "react";
+import { Alert } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./loginSignupform.css";
+import { signUpUser } from "../../helpers/axioshelper";
+
 const linkStyle = {
   color: "green",
   textDecoration: "underline",
   cursor: "pointer",
 };
+const initialState = {
+  fName: "dipendra",
+  lName: "paudel",
+  email: "dipendra25@gmail.com",
+  dob: "1999-08-01",
+  phone: "0452450077",
+  password: "dipak1234",
+  address: "123 martin street",
+};
 const SignUpForm = () => {
+  const [form, setForm] = useState(initialState);
+  const [error, setError] = useState(false);
+  const [response, setResponse] = useState();
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+    setError(false);
+  };
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    if (form.password !== form.confirmPassword) {
+      return setError(true);
+    }
+    setError(false);
+    const { confirmPassword, ...rest } = form;
+    const { data } = await signUpUser(rest);
+    setResponse(data);
+  };
+
   return (
     <div>
-      <form className="loginsignupform">
+      <form className="loginsignupform" onSubmit={handleOnSubmit}>
         <h6 className="py-2">Login details</h6>
         <div className="login--form__email py-2">
-          <input type="email" placeholder="Email address" name="email" />
+          <input
+            type="email"
+            placeholder="Email address"
+            name="email"
+            value={form.email}
+            required
+            onChange={handleOnChange}
+          />
         </div>
         <span style={{ fontSize: "0.8em" }}>
           <i className="fa fa-solid fa-info-circle text-primary"></i> we use
@@ -23,36 +66,85 @@ const SignUpForm = () => {
             type="password"
             placeholder=" Create password"
             name="password"
+            value={form.password}
+            required
+            onChange={handleOnChange}
           />
         </div>
+        <div className="login--form__password py-2">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            value={form.confirmPassword}
+            required
+            onChange={handleOnChange}
+          />
+        </div>
+        <Alert variant="danger" show={error}>
+          password donot match
+        </Alert>
         <hr />
         <h6 className="py-2">Personal details</h6>
         <div className="login--form__email py-2">
-          <input type="text" placeholder="First Name" name="fName" required />
+          <input
+            type="text"
+            placeholder="First Name"
+            name="fName"
+            required
+            value={form.fName}
+            onChange={handleOnChange}
+          />
         </div>
         <div className="login--form__email py-2">
-          <input type="text" placeholder="Last Name" name="lName" required />
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="lName"
+            value={form.lName}
+            required
+            onChange={handleOnChange}
+          />
         </div>
         <div className="login--form__email py-2">
-          <input type="date" placeholder="DD/MM/YY" name="dob" required />
+          <input
+            type="date"
+            placeholder="DD/MM/YY"
+            name="dob"
+            value={form.dob}
+            onChange={handleOnChange}
+          />
         </div>
         <span style={{ fontSize: "0.8em" }}>
           <i className="fa fa-solid fa-info-circle text-primary"></i> we use
           this to regulate the purchase of restricted products.
         </span>
         <div className="login--form__email py-2">
-          <input type="tel" placeholder="Mobile Number" name="phone" />
+          <input
+            type="tel"
+            placeholder="Mobile Number"
+            name="phone"
+            value={form.phone}
+            required
+            onChange={handleOnChange}
+          />
         </div>
         <span style={{ fontSize: "0.8em" }}>
           <i className="fa fa-solid fa-info-circle text-primary"></i> we use
           this to verify your identity or to update you on your orders.
         </span>
-        <button
+
+        {response && (
+          <Alert variant={response.status === "success" ? "success" : "danger"}>
+            {response.message}
+            <Link to="/shop/securelogin">Login now</Link>
+          </Alert>
+        )}
+        <input
           className="btn btn-lg btn-success loginbutton mt-3"
           type="submit"
-        >
-          Submit
-        </button>
+          value="Submit"
+        />
       </form>
     </div>
   );
--- src/helpers/axioshelper.js
+++ src/helpers/axioshelper.js
@@ -2,14 +2,16 @@ import axios from "axios";
 const rootUrlAPI = "http://localhost:8000/api/v1";
 const productsAPI = rootUrlAPI + "/products";
 const categoriesAPI = rootUrlAPI + "/categories";
+const customerAPI = rootUrlAPI + "/customer";
 
 //there will be lots of repetition on doing individual axios get method
 //so i am creating this api, processor
-const apiProcessor = async ({ method, url }) => {
+const apiProcessor = async ({ method, url, dataobj }) => {
   try {
     const { data } = await axios({
       method,
       url,
+      data: dataobj,
     });
     return data;
   } catch (error) {
@@ -19,18 +21,18 @@ const apiProcessor = async ({ method, url }) => {
 };
 
 //to get all the categories from the external api
-export const getAllCategories = async () => {
+export const getAllCategories = () => {
   const url = categoriesAPI;
   return apiProcessor({ method: "get", url });
 };
 //to get all the sub categories based on parentCatId
-export const getSubCategoriesBasedOnParentCatId = async (parentCatId) => {
+export const getSubCategoriesBasedOnParentCatId = (parentCatId) => {
   const url = categoriesAPI + "/" + parentCatId;
   return apiProcessor({ method: "get", url });
 };
 
 //to get all the products from the external api
-export const getAllProducts = async () => {
+export const getAllProducts = () => {
   const url = productsAPI;
   return apiProcessor({ method: "get", url });
 };
@@ -47,5 +49,24 @@ export const getProductsByParentCatId = async (ids) => {
     return data;
   } catch (error) {
     console.log(error.mesage);
+  }
+};
+
+//=======user signup/ login
+export const signUpUser = async (dataobj) => {
+  try {
+    const result = await axios.post(customerAPI, dataobj);
+    return result;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const loginUser = async (dataobj) => {
+  try {
+    const url = customerAPI;
+    return apiProcessor({ method: "post", url, dataobj });
+  } catch (error) {
+    console.log(error);
   }
 };
